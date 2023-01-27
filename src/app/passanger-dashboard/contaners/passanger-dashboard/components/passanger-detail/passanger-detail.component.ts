f8a28ce7702fbbb9c6d8391a53d4466710eb744c
mahmoudcode <74220922+mahmoud913@users.noreply.github.com>
2023-01-27 09:55:37 -0600

    added edit button to passanger details

--- src/app/passanger-dashboard/contaners/passanger-dashboard/components/passanger-detail/passanger-detail.component.ts
+++ src/app/passanger-dashboard/contaners/passanger-dashboard/components/passanger-detail/passanger-detail.component.ts
@@ -10,19 +10,36 @@ import { Passenger } from "src/app/passanger-dashboard/models/passanger.interfac
           <span
           class="status"
           [class.checked-in]="detail.checkedIn"></span>
-        {{ detail.fullname }}
+          <div *ngIf="editing">
+          <input
+          type="text"
+          [value]="detail.fullname"
+          (input)="onNameChange(name.value)"
+          #name>
+          </div>
+          <div *ngIf="!editing">
+            {{ detail.fullname }}
+          </div>
         <div class="date">
           Check in date:
           {{ detail.checkInDate ? (detail.checkInDate | date: 'd MMMM y' | uppercase) : 'Not Checked in'}}
         </div>
         <div class="children">
           Children: {{ detail.children?.length || 0 }}
+          <button (click)="toggleEdit()">{{editing? 'Done' : 'Edit'}}</button>
       </div>
     `
 })
 export class PassangerDetailComponent {
   @Input()
   detail!: Passenger;
+  editing: boolean = false;
   constructor() {}
+  onNameChange(value: string){
+    this.detail.fullname = value
+  }
+  toggleEdit(){
+    this.editing = !this.editing
+  }
 
 }
