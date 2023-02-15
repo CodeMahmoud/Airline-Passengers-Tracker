124dad8f4f893e3a8bb0cc52d2eb847b347d59d2
CodeMahmoud <mahmoudcode913@gmail.com>
2023-02-15 01:32:27 -0600

    created dedicated components folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { PassengerDashboardComponent } from './passanger-dashboard/contaners/passanger-dashboard/passanger-dashboard.component';
+import { PassengerDashboardComponent } from './passenger-dashboard/contaners/passenger-dashboard/passenger-dashboard.component';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
   <div class="app">
-    <passanger-dashboard></passanger-dashboard>
+    <passenger-dashboard></passenger-dashboard>
   </div>
   `
 })
